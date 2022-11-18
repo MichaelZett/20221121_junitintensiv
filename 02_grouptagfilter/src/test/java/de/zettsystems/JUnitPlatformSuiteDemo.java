536e757cc63193e84d45d714902c43599d4b7439
MichaelZett <michael2.zoeller@gmail.com>
2022-11-18 11:02:39 +0100

    Remove solution

--- 02_grouptagfilter/src/test/java/de/zettsystems/JUnitPlatformSuiteDemo.java
+++ 02_grouptagfilter/src/test/java/de/zettsystems/JUnitPlatformSuiteDemo.java
@@ -1,12 +1,16 @@
 package de.zettsystems;
 
-import org.junit.platform.suite.api.*;
+import org.junit.platform.suite.api.ExcludeTags;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.SuiteDisplayName;
 
 @Suite
 @SuiteDisplayName("This is a suite")
-@SelectPackages({"de.zettsystems.fizzbuzz","de.zettsystems.legacy","de.zettsystems.grouped","de.zettsystems.common" })
+@SelectPackages({"de.zettsystems.fizzbuzz", "de.zettsystems.legacy", "de.zettsystems.grouped"})
 @SelectClasses({
-       MoreTest.class
+        MoreTest.class
 })
 @ExcludeTags("buzz")
 public class JUnitPlatformSuiteDemo {
